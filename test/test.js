01eb591f79e638399d812a1f4ef64d488e41f4bb
Rodrigo Bussolo <rbussolo91@gmail.com>
2017-09-17 22:42:11 -0400

    Esta sendo criado um jeito de testar a API, como não sei fazer ainda, só esta o layout xD

---
+++ test/test.js
@@ -0,0 +1,15 @@
+// Arquivo JS para realização de todos os teste referente a API rest (aqui também ficara os exemplos de utilização)
+
+new Vue({
+  el: '#testTipoOng',
+  data: {
+    tiposOng: []
+  }
+});
+
+new Vue({
+  el: '#testTipoArquivo',
+  data: {
+    tiposArquivo: []
+  }
+});
